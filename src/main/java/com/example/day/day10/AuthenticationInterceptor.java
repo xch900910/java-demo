c4b38e4bfe9a6dc11fce7b1428a3bb76a54321b4
zt15864 <xch900910>
2021-05-20 15:32:02 +0800

    添加统一日志处理，统一权限认证，统一异常处理，线程池，统一返回对象，请求对象

---
+++ src/main/java/com/example/day/day10/AuthenticationInterceptor.java
@@ -0,0 +1,134 @@
+package com.example.day.day10;
+
+import com.alibaba.fastjson.JSON;
+
+import com.example.day.day10.util.SignUtil;
+import com.google.common.base.Strings;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.util.*;
+
+import static com.example.day.day10.Constants.*;
+import static com.example.day.day10.util.CommonUtil.sort;
+import static com.example.day.day10.util.SignUtil.decodeBase64;
+import static com.example.day.day10.util.SignUtil.getPublicKey;
+
+
+/**
+ * @author: xiongchaohua
+ * @Des : 接口统一认证入口
+ * @create: 2021-05-19 15:19
+ **/
+@Component
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+    @Value("${auth.enable:false}")
+    private boolean authEnable;
+    @Value("${appKey:test}")
+    private String appKey;
+    @Value("${publicKey:test}")
+    private String publicKey;
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //全局控制
+        if (!authEnable) {
+            return true;
+        }
+        // 如果不是映射到方法直接通过
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Method method = handlerMethod.getMethod();
+        //检查是否有PassToken注释，有则跳过认证
+        if (method.isAnnotationPresent(SkipAuthVerify.class)) {
+            SkipAuthVerify skipAuthVerify = method.getAnnotation(SkipAuthVerify.class);
+            if (skipAuthVerify.required()) {
+                return true;
+            }
+        }
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        Map<String, String> headerMap = new HashMap<>();
+        while (headerNames.hasMoreElements()) {
+            String key = headerNames.nextElement();
+            String value = request.getHeader(key);
+            headerMap.put(key, value);
+        }
+        boolean signResult = verifyApiSign(headerMap, appKey, publicKey);
+        if (!signResult) {
+            Result result = new Result();
+            result.setCode(ResultCode.UNAUTHORIZED).setMessage("签名认证失败");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(JSON.toJSONString(result));
+        }
+        return signResult;
+    }
+
+    /**
+     * 验证签名算法
+     * 将请求头参数拼接，appKey=zt001&charset=UTF-8&sign_type=RSA2&timeStamp=1589279314 （参数按照字典顺序固定）
+     * 使用公钥验签
+     *
+     * @param params
+     * @param appKey
+     * @param publicKey
+     * @return
+     * @throws Exception
+     */
+    private boolean verifyApiSign(Map<String, String> params, String appKey, String publicKey) throws Exception {
+        // 1. 验证参数
+        if (null == params || params.isEmpty() || Strings.isNullOrEmpty(appKey) || Strings.isNullOrEmpty(publicKey)) {
+            return false;
+        }
+        //第三方传入的签名
+        String sign = null != params.get(SIGN) ? params.get(SIGN) : "";
+        //2. 验证是否缺少验签所需参数
+        Set<String> keys = params.keySet();
+        Iterator<String> iterator = keys.iterator();
+        //缺少随机时间戳参数
+        if (!keys.contains(TIME_STAMP)) {
+            return false;
+        }
+        //判断时间戳
+        if (System.currentTimeMillis() - Long.parseLong(params.get(TIME_STAMP)) >= SIGN_TTL
+                || Long.parseLong(params.get(TIME_STAMP)) > System.currentTimeMillis()) {
+            return false;
+        }
+        //循环校验参数的值是否为空
+        while (iterator.hasNext()) {
+            String paramName = iterator.next();
+            Object paramValue = params.get(paramName);
+            if ("".equals(paramName) && null == paramValue) {
+                return false;
+            }
+        }
+        //nginx默认忽略带下划线参数
+        params.putIfAbsent(SIGN_TYPE, SIGN_TYPE_VALUE);
+        String[] signatureParamsKeys = new String[]{APP_KEY_NAME, CHAR_SET, SIGN_TYPE, TIME_STAMP};
+        // 3.对参数进行排序
+        sort(signatureParamsKeys);
+
+        //组装参与签名生成的字符串(使用 URL 键值对的格式（即key1=value1&key2=value2…）拼接成字符串)
+        StringBuilder string = new StringBuilder();
+        for (int i = 0; i < signatureParamsKeys.length; i++) {
+            //拼接随机字符串、时间戳参数
+            String paramValue = null != params.get(signatureParamsKeys[i]) ? params.get(signatureParamsKeys[i]) : "";
+            string.append(signatureParamsKeys[i]).append("=").append(paramValue);
+
+            if (i != signatureParamsKeys.length - 1) {
+                string.append("&");
+            }
+        }
+        return SignUtil.verify256(string.toString(), decodeBase64(sign.getBytes()), getPublicKey(publicKey));
+    }
+
+}
---
+++ src/main/java/com/example/day/day10/util/SignUtil.java
@@ -0,0 +1,241 @@
+package com.example.day.day10.util;
+
+import javax.crypto.Cipher;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+/**
+ * @author: xiongchaohua
+ * @Des :
+ * @create: 2020-07-17 09:20
+ **/
+public class SignUtil {
+    private static final String ENCODING = "UTF-8";
+    private static final String SIGNATURE_ALGORITHM = "SHA256withRSA";
+    /**
+     * 加密算法RSA
+     */
+    public static final String KEY_ALGORITHM = "RSA";
+    /**
+     * RSA最大加密明文大小
+     */
+    private static final int MAX_ENCRYPT_BLOCK = 117;
+
+
+    /**
+     * SHA256WithRSA签名
+     *
+     * @param data
+     * @param privateKey
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeySpecException
+     * @throws InvalidKeyException
+     * @throws SignatureException
+     * @throws UnsupportedEncodingException
+     */
+    public static byte[] sign256(String data, PrivateKey privateKey) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException,
+            SignatureException, UnsupportedEncodingException {
+
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+
+        signature.initSign(privateKey);
+
+        signature.update(data.getBytes(ENCODING));
+
+        return signature.sign();
+    }
+
+    public static boolean verify256(String data, byte[] sign, PublicKey publicKey) {
+        if (data == null || sign == null || publicKey == null) {
+            return false;
+        }
+
+        try {
+            Signature signetcheck = Signature.getInstance(SIGNATURE_ALGORITHM);
+            signetcheck.initVerify(publicKey);
+            signetcheck.update(data.getBytes("UTF-8"));
+            return signetcheck.verify(sign);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+
+    /**
+     * 二进制数据编码为BASE64字符串
+     *
+     * @param bytes
+     * @return
+     */
+    public static String encodeBase64(byte[] bytes) {
+        Base64.Encoder encoder = Base64.getEncoder();
+        return encoder.encodeToString(bytes);
+    }
+
+    /**
+     * BASE64解码
+     *
+     * @param bytes
+     * @return
+     */
+    public static byte[] decodeBase64(byte[] bytes) {
+        byte[] result = null;
+        Base64.Decoder decoder = Base64.getDecoder();
+        try {
+            result = decoder.decode(bytes);
+        } catch (Exception e) {
+            return null;
+        }
+        return result;
+    }
+
+    /**
+     * String转私钥PrivateKey
+     *
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public static PrivateKey getPrivateKey(String key) throws Exception {
+        byte[] keyBytes;
+        Base64.Decoder decoder = Base64.getDecoder();
+        keyBytes = decoder.decode(key);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+        return privateKey;
+    }
+
+    /**
+     * String转公钥PublicKey
+     *
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public static PublicKey getPublicKey(String key) throws Exception {
+        byte[] keyBytes;
+        Base64.Decoder decoder = Base64.getDecoder();
+        keyBytes = decoder.decode(key);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+        return publicKey;
+
+    }
+
+    /**
+     * 公钥加密
+     *
+     * @param data      需要加密的数据
+     * @param publicKey 公钥字符串
+     * @return 加密后的数据
+     * @throws Exception
+     */
+    public static byte[] encryptByPublicKey(byte[] data, String publicKey) throws Exception {
+        byte[] keyBytes = decodeBase64(publicKey.getBytes());
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        Key publicK = keyFactory.generatePublic(x509KeySpec);
+        // 对数据加密
+        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        cipher.init(Cipher.ENCRYPT_MODE, publicK);
+        int inputLen = data.length;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int offSet = 0;
+        byte[] cache;
+        int i = 0;
+        // 对数据分段加密
+        while (inputLen - offSet > 0) {
+            if (inputLen - offSet > MAX_ENCRYPT_BLOCK) {
+                cache = cipher.doFinal(data, offSet, MAX_ENCRYPT_BLOCK);
+            } else {
+                cache = cipher.doFinal(data, offSet, inputLen - offSet);
+            }
+            out.write(cache, 0, cache.length);
+            i++;
+            offSet = i * MAX_ENCRYPT_BLOCK;
+        }
+        byte[] encryptedData = out.toByteArray();
+        out.close();
+        return encryptedData;
+    }
+
+    /**
+     * 公钥加密方法
+     *
+     * @param data      需加密的字符串
+     * @param PUBLICKEY 公钥字符串
+     * @return 加密后的字符串
+     */
+    public static String encryptedDataByPublic(String data, String PUBLICKEY) {
+        try {
+            data = encodeBase64(encryptByPublicKey(data.getBytes(), PUBLICKEY));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    /**
+     * 私钥解密方法
+     *
+     * @param data       公钥加密的字符串
+     * @param PRIVATEKEY 私钥字符串
+     * @return 私钥解密的字符串
+     */
+    public static String decryptDataByPrivate(String data, String PRIVATEKEY) {
+        String temp = "";
+        try {
+            byte[] rs = decodeBase64(data.getBytes());
+            //以utf-8的方式生成字符串
+            temp = new String(decryptByPrivateKey(rs, PRIVATEKEY), "UTF-8");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return temp;
+    }
+
+    /**
+     * 私钥解密
+     *
+     * @param encryptedData 公钥加密的数据
+     * @param privateKey    私钥字符串
+     * @return 私钥解密的数据
+     * @throws Exception
+     */
+    public static byte[] decryptByPrivateKey(byte[] encryptedData, String privateKey) throws Exception {
+        PrivateKey prikey = getPrivateKey(privateKey);
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, prikey);
+        InputStream ins = new ByteArrayInputStream(encryptedData);
+        ByteArrayOutputStream writer = new ByteArrayOutputStream();
+        //rsa解密的字节大小最多是128，将需要解密的内容，按128位拆开解密
+        byte[] buf = new byte[256];
+        int bufl;
+        while ((bufl = ins.read(buf)) != -1) {
+            byte[] block = null;
+
+            if (buf.length == bufl) {
+                block = buf;
+            } else {
+                block = new byte[bufl];
+                for (int i = 0; i < bufl; i++) {
+                    block[i] = buf[i];
+                }
+            }
+            writer.write(cipher.doFinal(block));
+        }
+
+        return writer.toByteArray();
+    }
+}
