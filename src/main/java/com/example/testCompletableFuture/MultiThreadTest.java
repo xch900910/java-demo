610ed5510e7d39b0596787d820ad8643e5e9f028
zt15864 <xch900910>
2021-06-25 19:07:47 +0800

    添加CompletableFutureTest测试

---
+++ src/main/java/com/example/testCompletableFuture/MultiThreadTest.java
@@ -0,0 +1,64 @@
+package com.example.testCompletableFuture;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @author: xiongchaohua
+ * @Des :
+ * @create: 2021-06-25 15:53
+ **/
+
+public class MultiThreadTest {
+    /**
+     * 初始化4个商店
+     */
+    static List<Shop> shops = Arrays.asList(
+            new Shop("BestPrice"),
+            new Shop("LetsSaveBig"),
+            new Shop("MyFavoriteShop"),
+            new Shop("BuyItAll"));
+
+    public static List<Double> findPrices(String product, List<Shop> shops) {
+        return shops.stream().map(shop -> shop.getPrice(product)).collect(toList());
+    }
+
+    //    public static void main(String[] args) {
+//        long start = System.nanoTime();
+//        List<Double> prices = findPrices("肥皂", shops);
+//        //这里不可以做其他事情
+//        doSomethingElse();
+//        long invocationTime = ((System.nanoTime() - start) / 1_000_000);
+//        System.out.println("同步状态下查询价格耗时" + invocationTime + " ms," + "价格列表:" + prices);
+//    }
+    public static void main(String[] args) {
+        long start = System.nanoTime();
+        List<Future<Double>> pricesFuture = findPricesAsync("肥皂", shops);
+        // 这里可以做其他事情
+        doSomethingElse();
+        List<Double> prices = pricesFuture.stream()
+                .map(doubleFuture -> {
+                    try {
+                        return doubleFuture.get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(toList());
+        long invocationTime = ((System.nanoTime() - start) / 1_000_000);
+        System.out.println("异步状态下查询价格耗时" + invocationTime + " ms," + "价格列表:" + prices);
+    }
+
+    public static List<Future<Double>> findPricesAsync(String product, List<Shop> shops) {
+        return shops.stream().map(shop -> shop.getPriceAsync(product)).collect(toList());
+    }
+
+    private static void doSomethingElse() {
+        // 其他任务...
+        System.out.println("doSomethingElse");
+    }
+}
---
+++ src/main/java/com/example/testCompletableFuture/Shop.java
@@ -0,0 +1,60 @@
+package com.example.testCompletableFuture;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.apache.commons.lang3.RandomUtils;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: xiongchaohua
+ * @Des :
+ * @create: 2021-06-25 15:51
+ **/
+@Data
+@AllArgsConstructor
+public class Shop {
+    /**
+     * 商店名称
+     */
+    private String name;
+
+    /**
+     * 获取商品价格(同步)
+     */
+    public double getPrice(String product) {
+        // 会查询商店的数据库，但也有可能执行一些其他耗时的任务，比如联系其
+        // 他外部服务（比如，商店的供应商，或者跟制造商相关的推广折扣）
+        // 模拟这些工作的耗时
+        try {
+            TimeUnit.MILLISECONDS.sleep(500 + RandomUtils.nextInt(10, 1000));
+        } catch (InterruptedException ignored) {
+        }
+        // 模拟价格
+        return RandomUtils.nextDouble(1, 10) * product.charAt(0) + product.charAt(1);
+    }
+
+    /**
+     * 获取商品价格(异步)
+     */
+    public Future<Double> getPriceAsync(String product) {
+        // 创建CompletableFuture对象，它会包含计算的结果
+        CompletableFuture<Double> futurePrice = new CompletableFuture<>();
+        new Thread(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(500 + RandomUtils.nextInt(10, 1000));
+                // 模拟价格
+                double price = RandomUtils.nextDouble(1, 10) * product.charAt(0) + product.charAt(1);
+                // 需长时间计算的任务结束并得出结果时，设置Future的返回值
+                futurePrice.complete(price);
+            } catch (Exception ex) {
+                // 抛出导致失败的异常，完成这次Future操作
+                futurePrice.completeExceptionally(ex);
+            }
+        }).start();
+        // 无需等待还没结束的计算，直接返回Future对象
+        return futurePrice;
+    }
+}
