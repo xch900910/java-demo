1687faada12a45b583128b2adf7696fcb29c7b1e
xch900910 <314261077@qq.com>
2021-07-01 18:14:29 +0800

    添加completablefuture Test测试

--- src/main/java/com/example/completableFutureTest/CompletableFutureTest.java
+++ src/main/java/com/example/completableFutureTest/CompletableFutureTest.java
@@ -12,8 +12,23 @@ import java.util.concurrent.ExecutionException;
  **/
 public class CompletableFutureTest {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        CompletableFuture cf = new CompletableFuture();
-        cf.complete("code ending");
-        System.out.println(cf.get());
+//        CompletableFuture cf = new CompletableFuture();
+//        cf.complete("code ending");
+//        System.out.println(cf.get());
+        CompletableFuture.runAsync(() -> {
+        }).thenRun(() -> {
+        });
+        CompletableFuture.runAsync(() -> {
+        }).thenAccept(resultA -> {
+        });
+        CompletableFuture.runAsync(() -> {
+        }).thenApply(resultA -> "resultB");
+
+        CompletableFuture.supplyAsync(() -> "resultA").thenRun(() -> {
+        });
+        CompletableFuture.supplyAsync(() -> "resultA").thenAccept(resultA -> {
+        });
+        CompletableFuture<String> apply = CompletableFuture.supplyAsync(() -> "resultA").thenApply(resultA -> resultA + " resultB");
+        System.out.println(apply.get());
     }
 }
