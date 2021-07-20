75c6ad19153589a6d9032c28778752f232c84e83
xch900910 <314261077@qq.com>
2021-07-20 15:51:30 +0800

    添加启动停止shell脚本

--- src/main/java/com/example/jenkins/startup.sh
+++ src/main/java/com/example/jenkins/startup.sh
@@ -3,41 +3,41 @@ source /etc/profile
 
 springProfilesActive=$1
 SERVER_PORT=$2
-if [ "${springProfilesActive}" == '' ];then
-        echo "请输入参数指定spring.profiles.active的值"
-        exit 1
+if [ "${springProfilesActive}" == '' ]; then
+  echo "请输入参数指定spring.profiles.active的值"
+  exit 1
 fi
-if [ "${SERVER_PORT}" == '' ];then
-    SERVER_PORT="8080"
+if [ "${SERVER_PORT}" == '' ]; then
+  SERVER_PORT="8080"
 fi
 #进入项目目录
 cd /usr/local/master-data
 #获取启动项目jar包
-jar_name=`ls |grep master-data-`
+jar_name=$(ls | grep master-data-)
 
 #判断端口是否被占用
 if [ -n "$SERVER_PORT" ]; then
-    SERVER_PORT_COUNT=`netstat -tln | grep $SERVER_PORT | wc -l`
-    if [ $SERVER_PORT_COUNT -gt 0 ]; then
-        echo "ERROR: 服务的端口： $SERVER_PORT already used!"
-        exit 1
-    fi
+  SERVER_PORT_COUNT=$(netstat -tln | grep $SERVER_PORT | wc -l)
+  if [ $SERVER_PORT_COUNT -gt 0 ]; then
+    echo "ERROR: 服务的端口： $SERVER_PORT already used!"
+    exit 1
+  fi
 fi
 
 #启动jar
 
-nohup java -jar ${jar_name} --spring.profiles.active=${springProfilesActive} --server.port=${SERVER_PORT}  > /dev/null  2>&1 &
+nohup java -jar ${jar_name} --spring.profiles.active=${springProfilesActive} --server.port=${SERVER_PORT} >/dev/null 2>&1 &
 #判断是否成功启动
 COUNT=0
 while [ $COUNT -lt 1 ]; do
-    echo -e ".\c"
-    sleep 1
-    if [ -n "$SERVER_PORT" ]; then
-        COUNT=`netstat -an | grep $SERVER_PORT | wc -l`
-    fi
-    if [ $COUNT -gt 0 ]; then
-        break
-    fi
+  echo -e ".\c"
+  sleep 1
+  if [ -n "$SERVER_PORT" ]; then
+    COUNT=$(netstat -an | grep $SERVER_PORT | wc -l)
+  fi
+  if [ $COUNT -gt 0 ]; then
+    break
+  fi
 done
 
 echo "启动成功"
--- src/main/java/com/example/jenkins/stop.sh
+++ src/main/java/com/example/jenkins/stop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-jar_name=`ls |grep master-data-`
-isServerExist=` ps -ef|grep master-data- |grep -v "grep"|wc -l`
+jar_name=$(ls | grep master-data-)
+isServerExist=$(ps -ef | grep master-data- | grep -v "grep" | wc -l)
 if [ "$isServerExist" = "1" ]; then
-    pid=`ps -ef|grep master-data- |grep -v "grep" | awk '{print $2}'`
-    kill -9  $pid
-    echo "服务已停止"
+  pid=$(ps -ef | grep master-data- | grep -v "grep" | awk '{print $2}')
+  kill -9 $pid
+  echo "服务已停止"
 fi
