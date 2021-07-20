8a058b7d3fde05a25150bea897f7011d4c9d3ef5
xch900910 <314261077@qq.com>
2021-07-20 15:50:52 +0800

    添加启动停止shell脚本

---
+++ src/main/java/com/example/jenkins/stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+jar_name=`ls |grep master-data-`
+isServerExist=` ps -ef|grep master-data- |grep -v "grep"|wc -l`
+if [ "$isServerExist" = "1" ]; then
+    pid=`ps -ef|grep master-data- |grep -v "grep" | awk '{print $2}'`
+    kill -9  $pid
+    echo "服务已停止"
+fi
